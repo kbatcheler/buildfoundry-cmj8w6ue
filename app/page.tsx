eb07052d79844691e16886f837527c10aa90e8f7
kbatcheler <65650025+kbatcheler@users.noreply.github.com>
2025-12-16 18:05:28 +0000

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from '../components/hero-section';
+import FeatureCards from '../components/feature-cards';
+
+export default function HomePage() {
+  return (
+    <div>
+      <HeroSection />
+      <FeatureCards />
+    </div>
+  );
+}
