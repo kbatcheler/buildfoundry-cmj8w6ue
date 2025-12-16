4f6b3cfe8d4a132d063e8216490bc668e60aa68e
kbatcheler <65650025+kbatcheler@users.noreply.github.com>
2025-12-16 18:05:30 +0000

    Add components/footer.tsx

---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-200 dark:bg-gray-800 py-4 text-center">
+      <p>&copy; 2023 Render E2E Test. All rights reserved.</p>
+    </footer>
+  );
+}
